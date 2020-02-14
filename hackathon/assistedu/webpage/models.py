4dcb0a2dc5d8b297df3936dccc9d7a029d871f56
sreekar9601 <55232902+sreekar9601@users.noreply.github.com>
2020-02-15 00:19:38 +0530

    django

---
+++ hackathon/assistedu/webpage/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Disability(models.Model):
+    typeof=models.CharField(max_length=200)
+    description=models.CharField(max_length=200,default='hello')
+    pic=models.ImageField(upload_to='Disability/',default='blindkid.jpg')
+    myurl = models.URLField(max_length = 200, default='mute.html/')
+  
+    def __str__(self):
+        return self.typeof
+        return self.description
+        return self.myurl
+class Subject(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
