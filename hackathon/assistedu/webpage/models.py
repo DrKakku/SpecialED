9b2d4bbf14965ff2b5553d9796d5f950b30e57f9
sreekar9601 <55232902+sreekar9601@users.noreply.github.com>
2020-02-15 08:41:14 +0530

    major update

--- hackathon/assistedu/webpage/models.py
+++ hackathon/assistedu/webpage/models.py
@@ -5,6 +5,7 @@ class Disability(models.Model):
     description=models.CharField(max_length=200,default='hello')
     pic=models.ImageField(upload_to='Disability/',default='blindkid.jpg')
     myurl = models.URLField(max_length = 200, default='mute.html/')
+    audio=models.FileField(upload_to='media/',default='mute.mp3')
   
     def __str__(self):
         return self.typeof
@@ -12,5 +13,6 @@ class Disability(models.Model):
         return self.myurl
 class Subject(models.Model):
     name=models.CharField(max_length=200)
+    photo=models.ImageField(upload_to='InternalPics/',default='blindkid.jpg')
     def __str__(self):
         return self.name
